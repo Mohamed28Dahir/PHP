7b5a9ce72d274605dd5f6af7cdafaba7c02d40e8
Mohamed28Dahir <mohameddahirosman26@gmail.com>
2025-09-26 16:48:52 +0300

    Add files via upload

---
+++ 2.Assignment/1.Class Assignment/1.Ass.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color:blue;
+            text-align:center;
+        }
+        h2{
+            color:green
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "<h1>Jamuriya University Of Sciesce & Technology</h1>";
+    echo "<h2>About Information</h2>";
+    echo "<p>Jamhuriya University of Science and Technology (JUST) is private institution and was officially established in Mogadishu, Somalia, in 2011 by a group of Somali scholars and Intellectuals to fill the existing vacuum (in the quality) 
+    in the higher education system in Somalia by providing higher education of international standards.</p>";
+    echo "<h2>My Contact</h2>";
+    echo"<p><b>Number</b>:+252 612 223999</p>";
+    echo"<p><b>Email</b>:info@just.edu.so</p>";
+    echo"<p><b>Address</b>:Digfer Street, Hodan District</p>";
+    echo"<p><b>Website</b>:<a href='https://www.just.edu.so/'>Visit Website</p>";
+?>
+</body>
+</html>
