bcd4bc130f16da4c28daa9f7f3fd2efa9e968c88
Mohamed28Dahir <mohameddahirosman26@gmail.com>
2025-09-26 17:21:38 +0300

    Delete 3.Projects/1.Portfolio directory

--- 3.Projects/1.Portfolio/partials/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="site-footer">
-    <div class="container">
-        <p>&copy; <?= date('Y'); ?> CIT Student Portfolio. Built with PHP, HTML & CSS.</p>
-    </div>
-</footer>
-</body>
-</html>
-
